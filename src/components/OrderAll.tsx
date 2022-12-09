ee0d6b302f6bdda409d905e0397f48475a838dc5
Your Name <ioko.peezaza@gmail.com>
2022-12-09 16:27:24 +0700

    commit

--- src/components/OrderAll.tsx
+++ src/components/OrderAll.tsx
@@ -73,7 +73,7 @@ export function OrderAll() {
                                 <input id="custom_date" type="radio" name="order_filter" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                                 <label htmlFor="custom_date" className="font-bold ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">เลือกวันที่</label>
                             </div>
-                            <div className="flex items-center w-auto border rounded-lg w-64">
+                            <div className="flex items-center w-auto border rounded-lg w-auto">
                                 <Datepicker
                                     primaryColor="indigo"
                                     i18n="th"
