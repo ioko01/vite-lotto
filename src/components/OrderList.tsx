dba1c53d5aaa2962b2bb3aa3120c8ef8aa53e4ab
unknown <ioko.peezaza@gmail.com>
2022-12-13 00:25:21 +0700

    commit

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import Datepicker from "react-tailwindcss-datepicker";
+import { increment, incrementByAmount } from "../redux/actions/modalSlice";
+import { ModalDelete } from "./ModalDelete";
+
 
 type TypeDate = {
     startDate: string | Date | null,
@@ -126,7 +129,7 @@ export function OrderList() {
                                         <td className="border border-slate-300 font-light">พี่อิน</td>
                                         <td className="border border-slate-300 font-light">
                                             <div className="flex flex-row justify-around items-center">
-                                                <button className="text-[blue] hover:text-blue-500 hover:bg-gray-100">ดูรายละเอียด</button>
+                                                <button className="text-[blue] hover:text-blue-500 hover:bg-gray-100" onClick={() => incrementByAmount(true)}>ดูรายละเอียด</button>
                                                 <button className="text-xs text-red-600 hover:text-red-400 font-bold p-2 rounded shadow mx-2">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -148,6 +151,7 @@ export function OrderList() {
                     </div>
                 </div>
             </div>
+            <ModalDelete />
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './assets/fonts/FC_Minimal_Regular.ttf'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
